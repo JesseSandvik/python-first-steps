367446075f706cbd4147d528d492f5f9e010e4be
Jesse Sandvik <sandvikjesse@gmail.com>
2022-11-28 22:41:47 -0500

    classExercise: extended User class into Admin, called inherited method

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from user import User
 
 username: str = "ghostiefloor123"
 password: str = "floofB00f@11"
-new_user = User()
-
-new_user.username = username
-new_user.password = password
+new_user = User(username, password)
 
 new_user.greet()
+
+
+class Admin(User):
+    pass
+
+
+bossman = Admin("Bossman", "bossmanPasswordStuff")
+bossman.greet()
